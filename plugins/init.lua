150c60f93f376aa85f8da5deb1f646d0b890eea5
klukaszek <kylelukaszek@gmail.com>
2024-11-13 04:31:57 -0500

    Updated config to setup treesitter on fresh boots. Added markdown rendering.

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,92 +1,104 @@
 local plugins = {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-        require("nvchad.configs.lspconfig").defaults()
-        require "configs.lspconfig"
-        end,
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "jdtls",
+        "html-lsp",
+        "css-lsp",
+        "typescript-language-server",
+        "wgsl-analyzer",
+        "tailwindcss-language-server",
+        "rust-analyzer",
+        "lua-language-server",
+        "marksman",
+        "python-lsp-server",
+        "pylint",
+        "glow",
+        "ltex-ls",
+      },
     },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "clangd",
-                "jdtls",
-                "html-lsp",
-                "css-lsp",
-                "typescript-language-server",
-                "wgsl-analyzer",
-                "tailwindcss-language-server",
-                "rust-analyzer",
-                "lua-language-server",
-                "marksman",
-                "python-lsp-server",
-                "pylint",
-                "glow",
-                "ltex-ls",
-            },
+  },
+  {
+    "mbbill/undotree",
+    lazy = false,
+    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo Tree" }),
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        filetypes = {
+          java = true,
+          flex = true,
+          ["*.flex"] = true,
+          ["*.cup"] = true,
+          ["*.wgsl"] = true,
         },
-    },
-    {
-        "mbbill/undotree",
-        lazy = false,
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo Tree" }),
-    },
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                filetypes = {
-                    java = true,
-                    flex = true,
-                    ["*.flex"] = true,
-                    ["*.cup"] = true,
-                    ["*.wgsl"] = true,
-                },
-                panel = {
-                    enabled = true,
-                    auto_refresh = true,
-                    layout = {
-                        position = "right",
-                        ratio = 0.3,
-                    },
-                },
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    debounce = 100,
-                    keymap = {
-                        accept = "<M-l>",
-                        accept_word = false,
-                        accept_line = false,
-                        next = "<M-]>",
-                        prev = "<M-[>",
-                        dismiss = "<C-]>",
-                    },
-                },
-            })
-        end,
-
-    },
-    {
-        "vim-scripts/cup.vim",
-        lazy = false,
-    },
-    {
-        "meanderingprogrammer/render-markdown.nvim",
-        lazy = false,
-        config = function()
-            require('render-markdown').setup({})
-        end,
-    }
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          layout = {
+            position = "right",
+            ratio = 0.3,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 100,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      }
+    end,
+  },
+  {
+    "vim-scripts/cup.vim",
+    lazy = false,
+  },
+  {
+    "meanderingprogrammer/render-markdown.nvim",
+    lazy = false,
+    config = function()
+      require("render-markdown").setup {}
+    end,
+  },
 }
 
+-- import treesitter.parsers
+local parsers = require("nvim-treesitter.parsers")
+
+-- if parsers basic parsers are installed, skip the treesitter setup
+-- for whatever reason, treesitter keeps trying to install parsers even if they are already installed
+if parsers.get_parser_configs().bash then
+    if parsers.get_parser_configs().c then
+        if parsers.get_parser_configs().python then
+            if parsers.get_parser_configs().lua then
+                return plugins
+            end
+        end
+    end
+end
+
 -- Treesitter Setup for my commonly used languages
-require('nvim-treesitter.configs').setup {
-  -- A list of parser names, or "all" (supported parsers can be found here: 
-  -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages)
+require("nvim-treesitter.configs").setup {
   ensure_installed = {
     "markdown",
     "markdown_inline",
@@ -109,26 +121,16 @@ require('nvim-treesitter.configs').setup {
     "glsl",
     "hlsl",
     "slang",
-    "sql"
-    -- Add any other languages you commonly use
+    "sql",
   },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
   auto_install = true,
-
   highlight = {
     enable = true,
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
     additional_vim_regex_highlighting = true,
   },
-
   indent = {
-    enable = true
+    enable = true,
   },
 }
-
 return plugins
